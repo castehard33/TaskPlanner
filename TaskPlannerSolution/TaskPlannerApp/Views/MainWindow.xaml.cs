2e19cbfc57b0f10bd82fb30d51df0a7bdb5e87d3
Bartłomiej Mędrzak <bartlomiej.medrzak@gmail.com>
2025-03-17 23:40:17 +0100

    Poprawka foldery

--- TaskPlannerSolution/TaskPlannerApp/Views/MainWindow.xaml.cs
+++ TaskPlannerSolution/TaskPlannerApp/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TaskPlannerApp
+namespace TaskPlannerApp.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
