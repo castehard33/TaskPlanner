9b120795ab92b8a0d9869fc44daae2c0b84ac57d
Bartłomiej Mędrzak <bartlomiej.medrzak@gmail.com>
2025-03-19 13:48:55 +0100

    button fix

--- TaskPlannerSolution/TaskPlannerApp/Models/MainViewModel.cs
+++ TaskPlannerSolution/TaskPlannerApp/Models/MainViewModel.cs
@@ -1,11 +1,38 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Windows.Input;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 
 namespace TaskPlannerApp.Models
 {
-    public class MainViewModel
+    // Klasa reprezentująca zadanie
+    public class TaskItem
     {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Status { get; set; }
+    }
 
+    // Klasa do zapisywania i wczytywania zadań w pliku JSON
+    public class TaskService
+    {
+        private const string FileName = "tasks.json";
+
+        // Zapisz listę zadań do pliku JSON
+        public void SaveTasks(List<TaskItem> tasks)
+        {
+            string json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+            File.WriteAllText(FileName, json);
+        }
+
+        // Wczytaj listę zadań z pliku JSON
+        public List<TaskItem> LoadTasks()
+        {
+            if (!File.Exists(FileName))
+                return new List<TaskItem>();
+
+            string json = File.ReadAllText(FileName);
+            return JsonConvert.DeserializeObject<List<TaskItem>>(json);
+        }
     }
 }
