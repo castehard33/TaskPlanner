3d6d20fe3cd57482b95f444f32be783b866dde39
Bartłomiej Mędrzak <bartlomiej.medrzak@gmail.com>
2025-04-02 00:03:04 +0200

    working refresh

--- TaskPlannerSolution/TaskPlannerApp/ViewModels/MainViewModel.cs
+++ TaskPlannerSolution/TaskPlannerApp/ViewModels/MainViewModel.cs
@@ -1,64 +1,111 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Windows.Input;
-using TaskPlannerApp.Data;
+using System.Linq;
 using System.Windows;
+using System.Windows.Input;
+using Microsoft.EntityFrameworkCore;
+using TaskPlannerApp.Data;
 using TaskPlannerApp.Models;
 using TaskPlannerApp.Views;
-using Microsoft.EntityFrameworkCore;
-
 
 namespace TaskPlannerApp.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-
         private readonly TaskPlannerContext _context;
+        private ObservableCollection<TaskModel> _tasksToDo;
+        private ObservableCollection<TaskModel> _tasksInProgress;
+        private ObservableCollection<TaskModel> _tasksDone;
 
         public event PropertyChangedEventHandler? PropertyChanged;
         protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-        public ObservableCollection<TaskModel> TasksToDo { get; set; } = [];
-        public ObservableCollection<TaskModel> TasksInProgress { get; set; } = [];
-        public ObservableCollection<TaskModel> TasksDone { get; set; } = [];
+        public ObservableCollection<TaskModel> TasksToDo
+        {
+            get => _tasksToDo;
+            set
+            {
+                _tasksToDo = value;
+                OnPropertyChanged(nameof(TasksToDo));
+            }
+        }
+
+        public ObservableCollection<TaskModel> TasksInProgress
+        {
+            get => _tasksInProgress;
+            set
+            {
+                _tasksInProgress = value;
+                OnPropertyChanged(nameof(TasksInProgress));
+            }
+        }
+
+        public ObservableCollection<TaskModel> TasksDone
+        {
+            get => _tasksDone;
+            set
+            {
+                _tasksDone = value;
+                OnPropertyChanged(nameof(TasksDone));
+            }
+        }
 
         public ICommand AddTaskCommand { get; }
-
         public ICommand MoveToInProgressCommand { get; }
-
-
 
         public MainViewModel()
         {
             _context = new TaskPlannerContext();
-            LoadTasks();
+            _tasksToDo = new ObservableCollection<TaskModel>();
+            _tasksInProgress = new ObservableCollection<TaskModel>();
+            _tasksDone = new ObservableCollection<TaskModel>();
+
+            LoadAllTasks();
 
             AddTaskCommand = new RelayCommand(OpenTaskForm);
             MoveToInProgressCommand = new RelayCommand(MoveTaskToInProgressCommand);
         }
 
-        private void LoadTasks()
+        private async void LoadAllTasks()
         {
-            TasksToDo = new ObservableCollection<TaskModel>(
-                _context.Tasks.Where(t => t.Status == AppTaskStatus.ToDo).ToList()
-            );
+            try
+            {
+                // Get tasks from database
+                var todoTasks = await _context.Tasks
+                    .Where(t => t.Status == AppTaskStatus.ToDo)
+                    .ToListAsync();
 
-            TasksInProgress = new ObservableCollection<TaskModel>(
-                _context.Tasks.Where(t => t.Status == AppTaskStatus.InProgress).ToList()
-            );
+                var inProgressTasks = await _context.Tasks
+                    .Where(t => t.Status == AppTaskStatus.InProgress)
+                    .ToListAsync();
 
-            TasksDone = new ObservableCollection<TaskModel>(
-                _context.Tasks.Where(t => t.Status == AppTaskStatus.Done).ToList()
-            );
+                var doneTasks = await _context.Tasks
+                    .Where(t => t.Status == AppTaskStatus.Done)
+                    .ToListAsync();
+
+                // Update collections on UI thread
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    // Replace collections entirely instead of modifying them
+                    TasksToDo = new ObservableCollection<TaskModel>(todoTasks);
+                    TasksInProgress = new ObservableCollection<TaskModel>(inProgressTasks);
+                    TasksDone = new ObservableCollection<TaskModel>(doneTasks);
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading tasks: {ex.Message}", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void OpenTaskForm(object? parameter)
         {
-
             var taskForm = new TaskFormWindow(
                taskName: "Enter task name",
                taskAuthor: "Enter task author"
-           );
+            );
 
             if (taskForm.ShowDialog() == true)
             {
@@ -73,7 +120,8 @@ namespace TaskPlannerApp.ViewModels
                     _context.Tasks.Add(newTask);
                     await _context.SaveChangesAsync();
 
-                    TasksToDo.Add(newTask);
+                    // Reload all tasks instead of just adding to the collection
+                    LoadAllTasks();
 
                     MessageBox.Show("Task successfully added to the database!",
                                     "Success",
@@ -96,7 +144,7 @@ namespace TaskPlannerApp.ViewModels
             {
                 try
                 {
-                    // Find the task in the database
+                    // Find and update task in database
                     var existingTask = await _context.Tasks
                         .FirstOrDefaultAsync(t => t.Id == task.Id);
 
@@ -107,25 +155,14 @@ namespace TaskPlannerApp.ViewModels
                         return;
                     }
 
-                    // Update status using the enum
+                    // Update status
                     existingTask.Status = AppTaskStatus.InProgress;
 
                     // Save changes
                     await _context.SaveChangesAsync();
 
-                    // Refresh entire collection
-                    var updatedTasks = await _context.Tasks
-                        .Where(t => t.Status == AppTaskStatus.ToDo)
-                        .ToListAsync();
-
-                    Application.Current.Dispatcher.Invoke(() =>
-                    {
-                        TasksToDo.Clear();
-                        foreach (var updatedTask in updatedTasks)
-                        {
-                            TasksToDo.Add(updatedTask);
-                        }
-                    });
+                    // Reload ALL tasks from database - this ensures UI is in sync with DB
+                    LoadAllTasks();
 
                     MessageBox.Show("Task moved to In Progress", "Success",
                         MessageBoxButton.OK, MessageBoxImage.Information);
